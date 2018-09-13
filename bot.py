ca1135625be81cccb14f1de49e84798267902bd9
Antonio Vivace <avivace4@gmail.com>
2018-09-13 12:36:15 +0200

    Comments, naming, improvements

--- bot.py
+++ bot.py
@@ -4,7 +4,11 @@ from queue import Queue
 from threading import Thread
 
 class misterBot():
-	def __init__(self, botToken, log, webhookURL=None):
+	def __init__(self,
+				 botToken,
+				 log,
+				 webhookURL=None):
+		
 		self.bot = Bot(botToken)
 		self.log = log
 		self.updateQueue = Queue()
@@ -12,17 +16,21 @@ class misterBot():
 		self.dispatcherThread = Thread(target = self.dispatcher.start,
 									   name = 'dispatcher',
 									   daemon= True)
-
+		log.debug('Dispatcher spawned')
 		self.registerHandlers()
+		log.debug('Handlers registered')
 		self.dispatcherThread.start()
 		log.info('Bot started')
 
 	def setWebhook(self):
 		self.bot.set_webhook(self.webhookURL)
+		log.info('Telegram webhook set')
 
+	# Commands Handlers
 	def registerHandlers(self):
 		self.dispatcher.add_handler(
 			CommandHandler('life', self.life))
 
+	# Commands methods
 	def life(self, bot, update):
 		bot.send_message(chat_id=update.message.chat_id, text='42')
--- main.py
+++ main.py
@@ -1,38 +1,46 @@
-from telegram import Update
 from telegram.ext import CommandHandler
 from telegram.ext import Updater
 from flask import Flask, request
-from configSetup import loadConfig
+from telegram import Update
 import logging
 import json
+
+# Local imports
+from config import loadConfig
 from bot import misterBot
 
+# Configure logging
 log = logging.getLogger('logger')
-
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-					 level=logging.INFO)
+					level=logging.DEBUG)
 
-log.info('test')
-
-## Configuration Setup
+# Load configuration
 config = loadConfig()
 
-# Telegram Bot
+# Instance Telegram Bot
 mybot = misterBot(config["botToken"], log)
 
-## Webhook listener
+# Webhook listener
 app = Flask(__name__)
 
+## Test route
 @app.route("/")
 def hello():
 	return "Pay no attention to that man behind the curtain!"
 
+## Telegram webhook endpoint
 @app.route("/webhook", methods=["POST"])
-def webhook_handler():
+def tg_webhook_handler():
 	update = Update.de_json(request.get_json(), mybot.bot)
 	mybot.updateQueue.put(update)
 	return "done"
 
+## Twitch webhook endpoint
+@app.route("/webhook", methods=["POST"])
+def twitch_webhook_handler():
+	return "done"
+
+## Start Flask
 app.run(host="0.0.0.0",
 		port=3000,
 		debug=True, 
