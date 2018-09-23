339df8ede5045ac4470fdcc761b329d0d54e9784
Denni Bevilacqua <dennib@live.it>
2018-09-23 20:43:15 +0200

    Markdown parse mode, Cleaned queries code, Nicer message output format

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from telegram.ext import Dispatcher, CommandHandler
-from telegram import Bot, Update
+from telegram import Bot, Update, ParseMode
 from queue import Queue
 from threading import Thread
 import sqlite3
@@ -45,21 +45,25 @@ class misterBot():
 		dbConn = sqlite3.connect('./db.sqlite')
 		c = dbConn.cursor()
 		# Execute a count of wantend rows
-		chat_id = (str(update.message.chat_id),)
-		c.execute('SELECT COUNT(*) FROM SUBSCRIPTIONS WHERE ChatID = ?',chat_id)
+		sql = 'SELECT COUNT(*) FROM SUBSCRIPTIONS WHERE ChatID = ?'
+		queryParams = (str(update.message.chat_id),)
+		c.execute(sql,queryParams)
 		# If we have at least one sub for current user...
 		if c.fetchone()[0] > 0:
 			# ...extract subscriptions from db and collect them in subs variable...
-			c.execute('SELECT * FROM SUBSCRIPTIONS WHERE ChatID = ? ', chat_id)
+			sql = 'SELECT * FROM SUBSCRIPTIONS WHERE ChatID = ?'
+			c.execute(sql, queryParams)
 			subs = c.fetchall()
 			# Build up the message for the user with retrieved subscriptions
 			message = "Here's a list of all of your subscriptions:\n"
 			for sub in subs:
-				message += "\n" + sub[2]
+				streamer = sub[2]
+				status = "*Active*" if sub[3] else "_Disabled_"
+				message += '\n' + streamer + ': ' + status + '\n---'
 		else:
 			# ...otherwise warn the user he has no subscriptions yet
 			message = "Sorry, it seems you have no subscriptions yet"
 		# Close db connection
 		dbConn.close()
 		# Respond to the user with "message"
-		bot.send_message(chat_id=update.message.chat_id, text=message)
+		bot.send_message(chat_id=update.message.chat_id, text=message, parse_mode=ParseMode.MARKDOWN)
