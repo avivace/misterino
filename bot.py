b80977aba51497e8b7f8571818ed550bc65ca84b
Denni Bevilacqua <dennib@live.it>
2018-09-24 10:23:35 +0200

    Raw implementation of bot Enable and Disable methods

--- bot.py
+++ bot.py
@@ -33,6 +33,10 @@ class misterBot():
 			CommandHandler('life', self.life))
 		self.dispatcher.add_handler(
 			CommandHandler('show', self.show))
+		self.dispatcher.add_handler(
+			CommandHandler('enable', self.enable, pass_args=True))
+		self.dispatcher.add_handler(
+			CommandHandler('disable', self.disable, pass_args=True))
 
 	## Commands methods
 	def life(self, bot, update):
@@ -44,16 +48,20 @@ class misterBot():
 		# Open db connection and crate db cursor
 		dbConn = sqlite3.connect('./db.sqlite')
 		c = dbConn.cursor()
+
 		# Execute a count of wantend rows
 		sql = 'SELECT COUNT(*) FROM SUBSCRIPTIONS WHERE ChatID = ?'
 		queryParams = (str(update.message.chat_id),)
 		c.execute(sql,queryParams)
+
 		# If we have at least one sub for current user...
 		if c.fetchone()[0] > 0:
-			# ...extract subscriptions from db and collect them in subs variable...
+			# ...extract subscriptions from db and collect them
+			# in subs variable...
 			sql = 'SELECT * FROM SUBSCRIPTIONS WHERE ChatID = ?'
 			c.execute(sql, queryParams)
 			subs = c.fetchall()
+
 			# Build up the message for the user with retrieved subscriptions
 			message = "Here's a list of all of your subscriptions:\n"
 			for sub in subs:
@@ -63,7 +71,91 @@ class misterBot():
 		else:
 			# ...otherwise warn the user he has no subscriptions yet
 			message = "Sorry, it seems you have no subscriptions yet"
+
 		# Close db connection
 		dbConn.close()
 		# Respond to the user with "message"
-		bot.send_message(chat_id=update.message.chat_id, text=message, parse_mode=ParseMode.MARKDOWN)
+		bot.send_message(
+			chat_id=update.message.chat_id, 
+			text=message,
+			parse_mode=ParseMode.MARKDOWN)
+
+	## Enable method
+	#  Activate a previously disabled subscription
+	def enable(self, bot, update, args):
+		if len(args) > 1:
+			message = 'Sorry, you can enable only one streamer at a time'
+			bot.send_message(chat_id=update.message.chat_id, text=message)
+		else:
+			streamer = args[0]
+
+			dbConn = sqlite3.connect('./db.sqlite')
+			c = dbConn.cursor()
+
+			sql = 'SELECT Active FROM SUBSCRIPTIONS WHERE ChatID = ? AND Sub = ?'
+			queryParams = (update.message.chat_id,streamer)
+			c.execute(sql,queryParams)
+
+			status = c.fetchone()[0]
+
+			if status == 1:
+				dbConn.close()
+				message = 'No worries! *{}* subscription is already *Active*'\
+				.format(streamer)
+				bot.send_message(
+					chat_id=update.message.chat_id, 
+					text=message,
+					parse_mode=ParseMode.MARKDOWN)
+			else:
+				sql = 'UPDATE SUBSCRIPTIONS SET Active = ? WHERE ChatID = ? AND Sub = ?'
+				queryParams = (1,update.message.chat_id,streamer)
+				c.execute(sql,queryParams)
+				dbConn.commit()
+				dbConn.close()
+
+				message = "Yeeeey! *{}* subscription has been *Activated*"\
+				.format(streamer)
+				bot.send_message(
+					chat_id=update.message.chat_id, 
+					text=message,
+					parse_mode=ParseMode.MARKDOWN)
+
+	## Disable method
+	#  Disable a subscription
+	def disable(self, bot, update, args):
+		if len(args) > 1:
+			message = 'Sorry, you can disable only one streamer at a time'
+			bot.send_message(chat_id=update.message.chat_id, text=message)
+		else:
+			streamer = args[0]
+
+			dbConn = sqlite3.connect('./db.sqlite')
+			c = dbConn.cursor()
+
+			sql = 'SELECT Active FROM SUBSCRIPTIONS WHERE ChatID = ? AND Sub = ?'
+			queryParams = (update.message.chat_id,streamer)
+			c.execute(sql,queryParams)
+
+			status = c.fetchone()[0]
+
+			if status == 0:
+				dbConn.close()
+				message = 'No problem, *{}* subscription was already _Disabled_'\
+				.format(streamer)
+				bot.send_message(
+					chat_id=update.message.chat_id,
+					text=message,
+					parse_mode=ParseMode.MARKDOWN)
+			else:
+				sql = 'UPDATE SUBSCRIPTIONS SET Active = ? WHERE ChatID = ? AND Sub = ?'
+				queryParams = (0,update.message.chat_id,streamer)
+				c.execute(sql,queryParams)
+				dbConn.commit()
+				dbConn.close()
+
+				message = "Ok! *{}* subscription has been _Disabled_"\
+				.format(streamer)
+				bot.send_message(
+					chat_id=update.message.chat_id,
+					text=message,
+					parse_mode=ParseMode.MARKDOWN)
